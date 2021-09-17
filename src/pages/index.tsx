5402b0c0d2e39b05101c5ea085ae499cc07ff777
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-09-17 14:27:49 -0300

    style: adjust layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import {
-  Flex, Box, Image, Text, Heading,
+  Flex, Box, Image, Text, Heading, Divider, Center,
 } from '@chakra-ui/react';
 
 export default function Home(): JSX.Element {
@@ -7,11 +7,20 @@ export default function Home(): JSX.Element {
     <Flex
       w="100vw"
       h="100vh"
-      justify="center"
+      flexDirection="column"
     >
-      <Box backgroundImage="/images/background.png" w="100vw" h="40vh" backgroundSize="cover">
+      <Box
+        backgroundImage="/images/background.png"
+        w="100vw"
+        h="260px"
+        backgroundSize="cover"
+      >
         <Flex justifyContent="space-between" mx="80">
-          <Flex width="37%" justifyContent="center" flexDirection="column">
+          <Flex
+            width="37%"
+            justifyContent="center"
+            flexDirection="column"
+          >
             <Heading fontSize="4xl" fontWeight="medium" mb="4">
               5 Continentes,
               infinitas possibilidades.
@@ -21,6 +30,34 @@ export default function Home(): JSX.Element {
           <Image src="/images/airplane.svg" />
         </Flex>
       </Box>
+
+      <Image
+        src="/images/travel_types.svg"
+        w="80vw"
+        h="30vh"
+        mt="4"
+        mx="auto"
+      />
+
+      <Center height="8">
+        <Divider
+          orientation="horizontal"
+          color="black"
+          borderBottom="4px"
+          width="20"
+          borderRadius="2px"
+        />
+      </Center>
+
+      <Center flexDirection="column" my="8">
+        <Heading fontSize="3xl" fontWeight="medium" mb="4" color="gray.700">
+          Vamos nessa?
+        </Heading>
+
+        <Heading fontSize="3xl" fontWeight="medium" mb="4" color="gray.700">
+          Então escolha seu continente
+        </Heading>
+      </Center>
     </Flex>
   );
 }
